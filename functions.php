8fb39a9c86b2b2302cb25ab61854babe8cfc196c
cianbyrne1010 <cian.byrne1010@gmail.com>
2026-04-25 19:17:54 -0400

    fix(MJM-242): bump asset version for nav hotfix (#36)
    Co-authored-by: Dex <cian.byrne1010+dex@gmail.com>

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'RR_VERSION', '2.0.0' );
+define( 'RR_VERSION', '2.0.1' );
 define( 'RR_THEME_DIR', get_template_directory() );
 define( 'RR_THEME_URI', get_template_directory_uri() );
 
